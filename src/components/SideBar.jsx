6a7dc91aafe3274640902cb03e407ddab0d43974
ErickfuentesIT <erickfuentes.it@gmail.com>
2025-10-03 16:50:36 -0600

    Seguimiento de app worldwise, implementación de nuevos componentes y css.modules

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,17 @@
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
+
+export default function SideBar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <Outlet />
+      <p>List of cities</p>
+      <Footer />
+    </div>
+  );
+}
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,3 +1,13 @@
+import AppNav from "../components/AppNav";
+import SideBar from "../components/SideBar";
+import Map from "../components/Map";
+import styles from "./AppLayout.module.css";
+
 export default function AppLayout() {
-  return;
+  return (
+    <div className={styles.app}>
+      <SideBar />
+      <Map />
+    </div>
+  );
 }
